1a1d3eec1b5bfecbeea7e27fc591908c79cc2cc6
mkhuzin <maratmail89@gmail.com>
2024-10-24 00:45:30 +0300

    commit 3

--- src/test/java/org/example/MainTests.java
+++ src/test/java/org/example/MainTests.java
@@ -1,28 +1,4 @@
 package org.example;
 
-import jakarta.jms.JMSException;
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.junit.jupiter.api.Test;
-import org.springframework.jms.core.JmsTemplate;
-
 class MainTests {
-
-	@Test
-	void sendMessage() throws JMSException {
-
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-				"artemis",
-				"artemis",
-				"tcp://localhost:61616"
-		);
-
-		JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
-
-		jmsTemplate.send(
-				"queue1",
-				s -> s.createTextMessage("test123")
-		);
-
-	}
-
 }
