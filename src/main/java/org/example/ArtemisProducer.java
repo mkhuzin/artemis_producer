eef078efc5b1c299e8534427d098f43eaa9da0ef
mkhuzin <maratmail89@gmail.com>
2024-12-18 18:21:09 +0300

    commit 6

--- src/main/java/org/example/ArtemisProducer.java
+++ src/main/java/org/example/ArtemisProducer.java
@@ -6,7 +6,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,17 +24,18 @@ public class ArtemisProducer {
 	@Transactional
 	public void send(Data data) {
 
+		if (!applicationContext.containsBean("jmsTemplate"))
+			return;
+
 		String payload = mapper.writeValueAsString(data);
 
-		if (applicationContext.containsBean("jmsTemplate")) {
-			try {
-				jmsTemplate.send(
-						"queue1",
-						new CustomMessageCreator(payload)
-				);
-			} catch (Exception e) {
-				log.error(e.getMessage());
-			}
+		try {
+			jmsTemplate.send(
+					"queue1",
+					new CustomMessageCreator(payload)
+			);
+		} catch (Exception e) {
+			log.error(e.getMessage());
 		}
 
 	}
--- src/main/java/org/example/ArtemisProducerConfig.java
+++ src/main/java/org/example/ArtemisProducerConfig.java
@@ -3,7 +3,6 @@ package org.example;
 import jakarta.jms.JMSException;
 import lombok.Getter;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +10,14 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageConverter;
 
 @Getter
 @EnableJms
 @Configuration
+@ConditionalOnProperty("artemis.producer.auto-startup")
 public class ArtemisProducerConfig {
 
 	@Bean
-	@ConditionalOnProperty("artemis.producer.auto-startup")
 	public ActiveMQConnectionFactory connectionFactory() throws JMSException {
 
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
@@ -35,13 +33,6 @@ public class ArtemisProducerConfig {
 	}
 
 	@Bean
-	@ConditionalOnBean(ActiveMQConnectionFactory.class)
-	public JmsTransactionManager transactionManager(ActiveMQConnectionFactory connectionFactory) {
-		return new JmsTransactionManager(connectionFactory);
-	}
-
-	@Bean
-	@ConditionalOnBean(ActiveMQConnectionFactory.class)
 	public JmsTemplate jmsTemplate(ActiveMQConnectionFactory connectionFactory) {
 
 		JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
@@ -53,37 +44,22 @@ public class ArtemisProducerConfig {
 	}
 
 	@Bean
-	@ConditionalOnBean({
-			ActiveMQConnectionFactory.class,
-			JmsTransactionManager.class,
-			JmsTransactionManager.class,
-			MessageConverter.class
-	})
-	public DefaultJmsListenerContainerFactory containerFactory(
-			ActiveMQConnectionFactory connectionFactory,
-			JmsTransactionManager transactionManager,
-			MessageConverter messageConverter
-	) {
+	public DefaultJmsListenerContainerFactory containerFactory(ActiveMQConnectionFactory connectionFactory) {
 
 		DefaultJmsListenerContainerFactory containerFactory = new DefaultJmsListenerContainerFactory();
 
 		containerFactory.setConnectionFactory(connectionFactory);
 
-		containerFactory.setTransactionManager(transactionManager);
+		containerFactory.setTransactionManager(new JmsTransactionManager(connectionFactory));
 
 		containerFactory.setConcurrency("1-1");
 
 		containerFactory.setSessionTransacted(true);
 
-		containerFactory.setMessageConverter(messageConverter);
+		containerFactory.setMessageConverter(new CustomMessageConverter());
 
 		return containerFactory;
 
-	}
-
-	@Bean
-	public MessageConverter messageConverter() {
-		return new CustomMessageConverter();
 	}
 
 }
